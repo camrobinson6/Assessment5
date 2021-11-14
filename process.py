bf2fc507a7b2f02e64a3b08cf98328c4cc0c8a57
Cameron Robinson <92333068+camrobinson6@users.noreply.github.com>
2021-11-13 21:05:48 -0600

    Add files via upload

---
+++ process.py
@@ -0,0 +1,12 @@
+log_file = open("um-server-01.txt")  # this is how you open a file 
+
+
+def sales_reports(log_file): #defining  function sales_reports, with log_file as the paramater
+    for line in log_file: # loop thru and filtering a file
+        line = line.rstrip() # rstrip() method returns a copy of the string with trailing characters removed 
+        day = line[0:3] #this is the line and locatin on the line
+        if day == "Mon":#if satement: day equals Monday (changed from Tuesday)
+            print(line) # print whats in that line 
+
+
+sales_reports(log_file)#calling the function Sales_reports back with the paramater.
